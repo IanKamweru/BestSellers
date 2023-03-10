195d1ec9e0d216a64d65b68988ab7f0068a3606a
IanKamweru <ikamweru24@amherst.edu>
2023-03-10 16:25:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bestsellers/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.bestsellers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bestsellers.R.id
+import com.example.bestsellers.BestSellerBooksFragment
+
+/**
+ * The MainActivity for the BestSellerList app.
+ * Launches a [BestSellerBooksFragment].
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val supportFragmentManager = supportFragmentManager
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(id.content, BestSellerBooksFragment(), null).commit()
+    }
+}
